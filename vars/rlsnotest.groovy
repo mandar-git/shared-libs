27fb23ba7c13fee057681f6731a56840cd4a75d4
mandar-git <dindorkar.mandar@gmail.com>
2022-06-22 06:42:16 +0530

    Create rlsnotest.groovy

---
+++ vars/rlsnotest.groovy
@@ -0,0 +1,23 @@
+import java.io.*;
+import groovy.io.*;
+import java.util.Calendar.*;
+import java.text.SimpleDateFormat
+import hudson.model.*
+
+@NonCPS
+def call(Map config=[:]){
+    def dir = new File(pwd());
+    
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+    { 
+    	writer -> 
+                dir.eachFileRecurse(FileType.ANY){ file ->
+    		if (file.isDirectory()){
+    			writer.writeLine(file.name);            
+    		}
+    		else
+    		{
+    			writer.writeLine('\t' + file.name + '\t' + file.length());
+    		}
+    }
+}
